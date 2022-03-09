3f892a2280fe37cb7fed074465234884be1b80e9
Chappik <artemijeorel@yandex.ru>
2022-03-09 21:49:57 +0500

    rere

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int a = 0, b = 0, vvod = 0;
+                Console.WriteLine("1-Введите а\n2-Введите Б\n3-Сложение\n4-Вычитание\n5-Умножение\n6-Деление");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
